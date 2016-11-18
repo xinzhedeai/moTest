a2cef102e32a3354642c1cd8ce8d76ca4332f24b
xinzhedeai <2291006094@qq.com>
2016-11-18 23:42:40 +0800

    easyui grid  导航栏图标

--- sysConfig/index.js
+++ sysConfig/index.js
@@ -1,7 +1,14 @@
 /**
  * name index.js
  * author haliluya
- * 2016-11-17 09:40:15
+ * 
+ * 
+ */
+/**
+ * @file 摸版
+ * @author HaLiLuYa
+ * @date: 2016-11-17 09:40:15
+ * @version: V1.0
  */
 "use strict"
 
@@ -11,13 +18,12 @@ jQuery(document).ready(function () {
 	
 });
 $(function(){
-	
-	//顶部导航切换
+	//左部导航切换
 	$("#demo-list li").click(function(){
 		$("#demo-list li.active").removeClass("active")
 		$(this).addClass("active");
 	})	
-		// Dock initialize
+	//功能按钮初始化
 	$('#dock').Fisheye(
 		{
 			maxWidth: 30,
@@ -36,45 +42,17 @@ $(function(){
 			position : 'top',
 			content: function(){
 				var cc = $(this).attr('alt');
-				console.info(cc);
-	//					console.info("哈利路亚");
 				return cc;
-			}
+			},
+			onShow: function(){
+				$(this).tooltip('tip').css({
+					backgroundColor: '#428bca',
+					color: '#f7f7f7'
+				});
+    		}
+			
 		})
  })		
-//	)
-//	});
-//	$('#dock img').load()
-
-//var imgdefereds = [];
-//$('#dock img').each(function() {
-//	var dfd = $.Deferred();
-//	$(this).bind('load', function() {
-//		dfd.resolve();
-//	}).bind('error', function() {
-//		//图片加载错误，加入错误处理
-//		// dfd.resolve();
-//	})
-//	if(this.complete) setTimeout(function() {
-//		var i = 0;
-//		console.info("完成了张数:"+ (i++));
-//		dfd.resolve();
-//	}, 1000);
-//	imgdefereds.push(dfd);
-//})
-//$.when.apply(null, imgdefereds).done(function() {
-////	callback();
-//	$('#dock img').tooltip({
-//		position : 'top',
-//		content: function(){
-//			var cc = $(this).attr('alt');
-//			console.info(cc);
-////					console.info("哈利路亚");
-//			return cc;
-//		}
-//	});
-//			
-//});
 	//page footer
 	$('body').append('<div class="footerWrapper">' +
 		'<div class="footer">' +
@@ -87,3 +65,58 @@ $(function(){
 		'</div>' +
 	'</div>');
 })
+
+$(function(){
+	$('.startDate, .endDate').datepickerJQueryUI();
+	
+	var columns = [{
+		field : 'ck',
+		checkbox : true
+	},{
+		field : 'productid',
+		title : '运单号',
+		sortable : true,
+		width　:　200
+	},{
+		field : 'productname',
+		title : '商品名',
+		sortable : true,
+		width　:　200
+	},{
+		field : 'status',
+		title : '状态',
+		sortable : true,
+		width　:　200
+	},{
+		field : 'listprice',
+		title : '价格',
+		sortable : true,
+		width　:　200
+	}],
+	options = {
+//		url : '../datagrid_data1.json',
+		columns : [columns],
+		pagination : true,
+		pageSize : $index.pageSize,
+		pageNumer : $index.pageNumber,
+		order : $index.order,
+		sort :　$index.sort,
+		fitColumns : true,
+		method :　'get',
+		crossPageSwitch : true,
+		conditionSwitch : true,
+		scrollSwitch : true,
+		
+	};
+	$($index.indexDatagrid).datagridEasyUI(options);
+	$($index.indexDatagrid).datagrid('loadData',{total:0,rows:[]});	
+})
+
+//请求体
+var $index = {
+	indexDatagrid : '#indexDatagrid',
+	pageSize : 10,
+	pageNumber : 1,
+	order : 'desc',
+	sort : 'listprice'
+}
