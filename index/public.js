ec2ba43a5991c40275c0501d06d0c4e6b3e3b7a7
xinzhedeai <2291006094@qq.com>
2016-11-21 07:37:09 +0800

    左侧栏js提取
    哈利路亚

--- index/public.js
+++ index/public.js
@@ -8,12 +8,63 @@
  * 
  */
 
-//初始化 左侧菜单栏
-jQuery(document).ready(function () {
-	jQuery("#jquery-accordion-menu").jqueryAccordionMenu();
-	
-});
+
 $(function(){
+	    //左侧栏动态写入
+    var a = [{
+		"title": "系统配置",
+		"link": "/sys-conf",
+		"subMenu": [{
+			"menu_id": "317208e8-7257-11e5-b1ca-00163e000884",
+			"title": "基本信息",
+			"link": "/sys-conf/basicinfo.html"
+		}, {
+			"menu_id": "31720a9d-7257-11e5-b1ca-00163e000884",
+			"title": "日本邮编",
+			"link": "/sys-conf/jp-code.html"
+		}]
+	}, {
+		"title": "关联公司",
+		"link": "/company",
+		"subMenu": [{
+			"menu_id": "1c0a1e94-7254-11e5-b1ca-00163e000884",
+			"title": "代理公司",
+			"link": "/company/agency.html"
+		}, {
+			"menu_id": "1c0a1e93-7254-11e5-b1ca-00163e000884",
+			"title": "网购公司",
+			"link": "/company/shoppingco.html"
+		}]
+	}, {
+		"title": "商品管理",
+		"link": "/commodity",
+		"subMenu": [{
+			"menu_id": "1c2b5r77-7254-11e5-b1ca-00163e000884",
+			"title": "商品信息管理",
+			"link": "/commodity/productInfo.html"
+		}]
+	}];
+	
+	localStorage.menu = JSON.stringify(a);
+	var menu = $.parseJSON(localStorage.menu);
+	var navMenuStr = '', subMenuStr = '';
+	for(var i=0,leni = menu.length; i<leni; i++){
+			navMenuStr += '<li class="firstLevel"><a href="#" class="oneLevelA">';
+			navMenuStr += menu[i].title;
+			navMenuStr += '</a>';
+			navMenuStr += '<ul class="submenu">';
+//			console.info(menu[i].length);
+			for(var j=0, lenj = menu[i].subMenu.length; j<lenj; j++){
+				console.info("haliluya");
+				navMenuStr += '<li><a href="'+ menu[i].subMenu[j].link +'" class="twoLevelA">' +menu[i].subMenu[j].title+ '</a></li>'
+			}
+			navMenuStr += '</ul>';//二级菜单结束
+			navMenuStr += '</li>';//一级菜单结束
+	}
+	
+	$('#demo-list').html(navMenuStr);
+	
+	
 	//左部导航切换
 	$("#demo-list li").click(function(){
 		$("#demo-list li.active").removeClass("active")
@@ -22,12 +73,12 @@ $(function(){
 	//功能按钮初始化
 	$('#dock').Fisheye(
 		{
-			maxWidth: 30,
+			maxWidth: 10,
 			items: 'a',
 //			itemsText: 'span',
 			container: '.dock-container',
-			itemWidth: 50,
-			proximity: 60,
+			itemWidth: 60,
+			proximity: 40,
 			alignment : 'left',
 			valign: 'bottom',
 			halign : 'center'
@@ -78,4 +129,10 @@ $(function(){
 //			forceParse: 0,
 //	        showMeridian: 1,
     });
+
 })
+//初始化 左侧菜单栏
+jQuery(document).ready(function () {
+	jQuery("#jquery-accordion-menu").jqueryAccordionMenu();
+	
+});
--- sysConfig/index.js
+++ sysConfig/index.js
@@ -7,60 +7,6 @@
 "use strict"
 
 $(function(){
-	
-	var a = [{
-		"title": "系统配置",
-		"link": "/sys-conf",
-		"subMenu": [{
-			"menu_id": "317208e8-7257-11e5-b1ca-00163e000884",
-			"title": "基本信息",
-			"link": "/sys-conf/basicinfo.html"
-		}, {
-			"menu_id": "31720a9d-7257-11e5-b1ca-00163e000884",
-			"title": "日本邮编",
-			"link": "/sys-conf/jp-code.html"
-		}]
-	}, {
-		"title": "关联公司",
-		"link": "/company",
-		"subMenu": [{
-			"menu_id": "1c0a1e94-7254-11e5-b1ca-00163e000884",
-			"title": "代理公司",
-			"link": "/company/agency.html"
-		}, {
-			"menu_id": "1c0a1e93-7254-11e5-b1ca-00163e000884",
-			"title": "网购公司",
-			"link": "/company/shoppingco.html"
-		}]
-	}, {
-		"title": "商品管理",
-		"link": "/commodity",
-		"subMenu": [{
-			"menu_id": "1c2b5r77-7254-11e5-b1ca-00163e000884",
-			"title": "商品信息管理",
-			"link": "/commodity/productInfo.html"
-		}]
-	}];
-	
-	localStorage.menu = JSON.stringify(a);
-	var menu = $.parseJSON(localStorage.menu);
-	var navMenuStr = '', subMenuStr = '';
-	for(var i=0,leni = menu.length; i<leni; i++){
-			navMenuStr += '<li class="firstLevel"><a href="#" class="oneLevelA">';
-			navMenuStr += menu[i].title;
-			navMenuStr += '</a>';
-			navMenuStr += '<ul class="submenu">';
-//			console.info(menu[i].length);
-			for(var j=0, lenj = menu[i].subMenu.length; j<lenj; j++){
-				console.info("haliluya");
-				navMenuStr += '<li><a href="'+ menu[i].subMenu[j].link +'" class="twoLevelA">' +menu[i].subMenu[j].title+ '</a></li>'
-			}
-			navMenuStr += '</ul>';//二级菜单结束
-			navMenuStr += '</li>';//一级菜单结束
-	}
-	
-	$('#demo-list').html(navMenuStr);
-	
 	var columns = [{
 		field : 'ck',
 		checkbox : true
